bedadb1abb3445ee970f6fc61d207f94257cadb4
Jonny Souza da Silva <jonny@Mayaras-Mini.lan>
2021-01-21 00:22:46 +0100

    EndPoint de desposito

--- api-contas/index.js
+++ api-contas/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json()); // support json encoded bodies
+app.use(express.urlencoded({ extended: false })); // support encoded bodies
+
 const mysql = require('mysql');
 
 // setup database
 db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',
-    database: 'desafio_dev'
+    password: 'password1234',
+    database: 'desafio-dev'
   })
 
 const port = 3000;
@@ -40,8 +43,9 @@ app.get('/conta/saldo/:idConta', (req, res) => {
   });
 
 app.post('/conta', (req, res) => {
-
+  
   let sql = `INSERT INTO contas(idPessoa, saldo, limiteSaqueDiario, flagAtivo, tipoConta, dataCriacao) VALUES (?)`;
+  
   let values = [
     req.body.idPessoa,
     req.body.saldo,
@@ -50,7 +54,7 @@ app.post('/conta', (req, res) => {
     req.body.tipoConta,
     date = new Date()
   ];
-  console.log(values);
+  
   db.query(sql, [values], function(err, data, fields) {
     if (err) throw err;
     res.json({
@@ -61,6 +65,65 @@ app.post('/conta', (req, res) => {
 
 })
 
+app.put('/conta/inativar/:idConta', (req, res) => {
+  let sql = 'UPDATE contas SET flagAtivo = 0 WHERE idConta =?';
+  
+  db.query(sql, req.params.idConta, function(err, data, fields) {
+    if (err) throw err;
+    res.json({
+      status: 200,
+      message: "Conta inativada com sucesso."
+    })
+  })
+});
+
+// Api de depositos
+app.post('/conta/deposito', (req, res) => {
+
+    let valor = req.body.valor;
+    const idConta = req.body.idConta;
+
+  new Promise ((resolve, reject) =>{
+    
+    let sql = 'SELECT saldo FROM contas WHERE idConta =?';
+    
+    db.query(sql, idConta, function(err, data, fields) {
+      if (err) reject(err);
+      resolve(data[0].saldo)
+
+    })
+  }).then(value => {
+      console.log(value);
+      let saldoAtualizado = atualizaSaldo(value, valor);
+      return saldoAtualizado
+  }).then(saldoAtualizado=> {
+    let sql2 = 'UPDATE contas SET saldo =? WHERE idConta =?';
+
+  let values = [
+    saldoAtualizado,
+    idConta
+  
+  ]
+  console.log('Values insinde the query ' + values)
+  
+  db.query(sql2, values, function(err, data, fields) {
+    if (err) throw err;
+    res.json({
+      status: 200,
+      message: "Desposito realizado com sucesso"
+    })
+  })
+  }).catch(err => {
+    console.log(err);
+  });
+    
+  
+});
+
+function atualizaSaldo (saldoAtual, valor){
+  console.log('SaldoAtual: ' + saldoAtual);
+  return saldoAtual + valor;
+}
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
